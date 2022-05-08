824e6551e6823789bdb64c978e58509a79ec05ab
ksks2012 <st890751999@gmail.com>
2022-05-08 23:09:39 +0800

    feat(8): fix when number repeated

--- 8.c
+++ 8.c
@@ -3,6 +3,18 @@
  */
 #include "./lib/cheader.h"
 
+int min_inorder(int *numbers, int left, int right) {
+	int result = numbers[left];
+
+	for(int i = left + 1; i <= right; i++) {
+		if(result > numbers[i]) {
+			result = numbers[i];
+		}
+	}
+
+	return result;
+}
+
 int min_number(int *numbers, int length) {
 	if(numbers == NULL || length < 0)
 		return -1;
@@ -18,18 +30,24 @@ int min_number(int *numbers, int length) {
 		}
 
 		mid = (left + right) / 2;
+		if(numbers[left] == numbers[mid] && numbers[left] && numbers[mid]) {
+			return min_inorder(numbers, left, right);
+		}
+
 		if(numbers[left] <= numbers[mid]) {
 			left = mid;
 		} else if(numbers[right] >= numbers[mid]) {
 			right = mid;
 		}
 	}
+
 	return numbers[mid];
 }
 
 int main() {
 	int numbers[5] = {3, 4, 5, 1, 2};
-	printf("%d\n", min_number(numbers, 5));
+	int numbers2[5] = {1, 1, 1, 0, 1};
+	printf("%d\n", min_number(numbers2, 5));
 
 	return 0;
 }
